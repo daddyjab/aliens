5efb9da84d00d05a583d48a75378f2bc2f43110b
Jeff Brown <39058795+daddyjab@users.noreply.github.com>
2019-03-01 12:25:10 -0600

    Setup of initial files completed

---
+++ static/js/aliens.js
@@ -0,0 +1,13 @@
+/****************************************************
+
+Homework Assignment:
+14-JavaScript and DOM Manipulation - Project Aliens
+
+@Author: Jeffery Brown (daddyjab)
+@Date: 2/xx/19
+@File: aliens.js
+
+ ****************************************************/
+
+// Data for UFO sitings
+var ufoTableData = ufo_sitings_data;
